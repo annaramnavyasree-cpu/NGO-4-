d7899939c8f55e27c02fe0eae990cf3abbf2bf4f
Navyasree <annaramnavyasree@gmail.com>
2026-03-24 22:43:53 +0530

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,17 @@
+"""
+Flask Extensions
+Centralized initialization of all Flask extensions
+"""
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_wtf.csrf import CSRFProtect
+from flask_mail import Mail
+
+# Initialize extensions
+db = SQLAlchemy()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+csrf = CSRFProtect()
+mail = Mail()
